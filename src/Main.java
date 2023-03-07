8719fb5c4f90fae9f11c10f1fd2bcd7d85ec75f0
angelsflyinhell <azami@namespace.media>
2023-03-07 16:52:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String> stringArrayList = new ArrayList<>();
 
+        // Kann nur Elemente vom Typ String enthalten
         Queue<String> stringQueue = new Queue<>();
 
+        // Kann alle Elemente beinhalten, die vom Typ Object erben.
         NonGenericQueue queue = new NonGenericQueue(); // Object
         queue.enqueue("Hi"); // String
 
-        String stringAusQueue = (String) queue.getFirst(); // No Strings?
-        Object objectAusQueue = queue.getFirst(); // Object by default
+        //                      | Hier casten wir ein Object zu einem String
+        String stringAusQueue = (String) queue.getFirst();
+
+        //                      | Ansonsten wuerden wir ein Object bekommen
+        Object objectAusQueue = queue.getFirst();
     }
 }
